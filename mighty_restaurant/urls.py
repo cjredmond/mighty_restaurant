fff73cdc5c44138296f9db93d67d715246828d5c
cjredmond <connor.redmond@gmail.com>
2016-10-20 15:26:42 -0400

    added url and class for index

--- mighty_restaurant/urls.py
+++ mighty_restaurant/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from utility.views import UserCreateView, ProfileUpdateView
+from utility.views import UserCreateView, ProfileUpdateView, IndexView # FoodCreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('django.contrib.auth.urls')),
     url(r'^create_user/$', UserCreateView.as_view(), name='user_create_view'),
-    url(r'^accounts/profile/$', ProfileUpdateView.as_view(), name='profile_view')
+    url(r'^accounts/profile/$', ProfileUpdateView.as_view(), name='profile_view'),
+    url(r'^$', IndexView, name="index_view"),
+    #url(r'^food/create/$', FoodCreateView.as_view(), name='food_create_view')
 ]
--- utility/views.py
+++ utility/views.py
@@ -5,9 +5,21 @@ from django.views import View
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.models import User
+from utility.models import Profile, Food, Table, Order
 # Create your views here.
 
 class UserCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = '/'
+
+class ProfileUpdateView(UpdateView):
+    template_name = "profile.html"
+    fields = ('status', )
+    success_url = reverse_lazy('profile_view')
+
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
+
+class IndexView(View):
+    template_name = "index.html"
