b2501b7cc2cc00a8190245c83a2fa6d4599b6665
rallen0150 <rallen0150@gmail.com>
2016-10-20 15:02:34 -0400

    got the templates

--- utility/views.py
+++ utility/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views import View
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.models import User
 # Create your views here.
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = '/'
